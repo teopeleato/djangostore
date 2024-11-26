16d156a30d3dce5b2ab1cd398d1aa8c2da62866b
Teodoro Peleato <teopeleato@gmail.com>
2024-11-26 14:06:51 +0000

    home url

--- djangostore/djangostore/urls.py
+++ djangostore/djangostore/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from eventos import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('eventos/', include('eventos.urls')),  # Incluir URLs de la app 'eventos'
+    path('', views.home, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
