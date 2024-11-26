ffe57eeb9e2629b8d98b3f88247687fce3c7717e
Teodoro Peleato <teopeleato@gmail.com>
2024-11-26 11:05:31 +0100

    Update settings.py

--- djangostore/djangostore/settings.py
+++ djangostore/djangostore/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure-n^mq^i#fl_7vj08d+wt%t2pdgh2de-h0(2lp9n-&3k2m7dgr-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://djangostore.i3a.es'
+]
 
 
 # Application definition
