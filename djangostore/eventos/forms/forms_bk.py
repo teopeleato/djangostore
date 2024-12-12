d733d7302c8d75eb494e15d70470f3c46635a035
Teodoro Peleato <teopeleato@gmail.com>
2024-12-12 13:44:48 +0000

    Formulario: maquetacion y separacion

--- djangostore/eventos/forms/forms_bk.py
+++ djangostore/eventos/forms/forms_bk.py
@@ -1,6 +1,11 @@
 from django import forms
-from .models import Evento
+from ..models import Evento
 from django.utils.safestring import mark_safe
+
+OPCIONES_COMIDAS = [ 
+    (1,'Yes'),
+    (0,'No'),
+]
 
 # Formulario del evento 1 **********************************************************************************************************************
 class PrecioFormEvento1(forms.Form):
@@ -75,20 +80,18 @@ class PrecioFormEvento3(forms.Form):
         (__standard_and_member, f'Standard and member - {__standard_and_member} € (-{__descuento_member}% inc.)'),
     ]
 
-    # Campos que definen el precio
+    # Campos que definen el precio ************************************************************************************************************************
 
     precioModality = forms.ChoiceField(
-        widget = forms.RadioSelect(attrs={
-            'class': 'p-3',
-        }),
+        widget = forms.RadioSelect(),
         choices = OPCIONES_PRECIOS, 
         initial = __standard,
         label = "Please select the corresponding option:",          
     )  
 
-    # Campos que no influyen en el precio, informativos
+    # Campos que no influyen en el precio, informativos ************************************************************
 
-    texto_informativo = forms.CharField(
+    """ texto_informativo = forms.CharField(
         initial="Please mark your optional options:",
         required=False,
         widget=forms.TextInput(attrs={
@@ -96,25 +99,46 @@ class PrecioFormEvento3(forms.Form):
             'class': 'form-control-plaintext',
         }),
         label="",
+    ) """
+
+    papers = forms.CharField(
+        required=False,
+        label="If you present paper/s, tell us the number ID of it or them.",
     )
 
-    comida = forms.BooleanField(
+    """ comida = forms.BooleanField(
         required = False,  
+        widget = forms.CheckboxInput(attrs={
+            'class': 'check-opcionales',
+        }),
         label = "Lunch 5 June", 
-        widget = forms.CheckboxInput(attrs={
-            'class': 'check-opcionales',
-        }),
     )  
+ """
 
-    cena = forms.BooleanField(
-        widget = forms.CheckboxInput(attrs={
-            'class': 'check-opcionales',
-        }),
-        required = False,  
-        label = "Dinner 5 June",         
+    comida = forms.ChoiceField(
+        widget = forms.RadioSelect(),
+        initial = 0,        
+        choices = OPCIONES_COMIDAS, 
+        label = "Are you attending the lunch on 5 June?", 
     )
 
-    # Precio final
+    cena = forms.ChoiceField(
+        widget = forms.RadioSelect(),
+        initial = 0,        
+        choices = OPCIONES_COMIDAS, 
+        label = "Are you attending the dinner on 6 June?", 
+    )
+
+    intolerancias = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'rows':3, 
+            'cols':35,
+        }),
+        label="Tell us if you want a vegan or vegetarian menu. And if you have any food allergy or intolerance.",
+    )
+
+    # Precio final ************************************************************************************************************************
 
     precioFinal = forms.IntegerField(
         required = False, 
